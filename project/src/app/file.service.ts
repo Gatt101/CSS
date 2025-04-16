47aa4008fd361e08eb11ed4d542b669af5e13b4d
Gatt101 <gauravpatilk11@gmail.com>
2025-04-16 23:33:15 +0530

    deploy

--- project/src/app/file.service.ts
+++ project/src/app/file.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FileService {
-  private apiUrl = 'https://css-gatt101s-projects.vercel.app';
+  private apiUrl = 'https://css-xpyd.onrender.com';
 
   constructor(private http: HttpClient) {}
 
